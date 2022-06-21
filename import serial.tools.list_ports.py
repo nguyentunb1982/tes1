329d34d794ddbab1a101508c39ed477ece21529e
nguyentunb1982 <63252341+nguyentunb1982@users.noreply.github.com>
2022-06-21 15:06:25 +0700

    Add files via upload

---
+++ import serial.tools.list_ports.py
@@ -0,0 +1,18 @@
+import serial.tools.list_ports
+import subprocess
+ports = list(serial.tools.list_ports.comports())
+for p in ports:
+    print (p)
+from tkinter import *
+def clicked():
+     
+    lbl.configure(text="Button was clicked !!")
+    subprocess.call("C:/Program Files (x86)/AtHomeVideoStreamer/AtHomeVideoStreamer.exe",shell=True)
+window = Tk()
+
+window.title("Welcome to LikeGeeks app")
+lbl = Label(window, text="Đi chợ", font=("Arial Bold", 50))
+btn = Button(window, text="Click Tôi", font =("Arial Bold", 10), bg="green", fg="red", command=clicked)
+btn.grid(column=1, row=0)
+lbl.grid(column=0, row=0)
+window.mainloop()
